2a2eb4323ffe3c227541b6fff74ad80bdf19fbb7
Mohnish Devadiga <mohnish226@yahoo.co.in>
2018-06-14 04:25:05 +0530

    HTML pages Working, also kept Debugger ON
    Check WhatsApp for further information. Did not check with the jQuery

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ Created on Sat Jun  9 21:11:30 2018
 
 """
 
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request
 from Extraction import extract_from_url
 from UrlCheck import url_check
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -21,13 +22,19 @@ def index():
 @app.route('/',methods=['POST'])
 @app.route('/index',methods=['POST'])
 def handle_req():
-    text=request.form['url']
-    if url_check(text):
-        result=extract_from_url(text)
-        return result
-    else:
-        return "invalid url try again"
-    
+    text = request.form['url']
+    result = extract_from_url(text)
+    return result
+'''
+def handle_req():
+    if request.method=='POST':
+        text=request.form['url']
+        if url_check(text):
+            result=extract_from_url(text)
+            return json.dumps({'result':result})
+        else:
+            return json.dumps({'result':"Invalid url try again"})
+'''
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
